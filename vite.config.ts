945af9da2c183ae6ec182f7ea11e86e46d17af26
Alex Beuscher <alexbeuscher@gmail.com>
2024-10-04 00:36:01 -0400

    Working on build and router issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { URL, fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
@@ -14,10 +14,17 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      input: {
-        main: './public/index.html',
-        secondPage: './public/gemini/index.html'
-      }
+      input: entryPoints('index.html', 'gemini/index.html')
     }
-  }
+  },
+  publicDir: 'public'
 })
+
+function entryPoints(...paths: string[]) {
+  const entries = paths.map((path) => {
+    const name = path.split('/').pop()?.replace('.html', '') || ''
+    return [name, resolve(__dirname, path)]
+  })
+
+  return Object.fromEntries(entries)
+}
